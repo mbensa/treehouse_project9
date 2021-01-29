f5eebfbd589283721ac59aa58c2befe83099015e
Mojca Bensa <mojca.bensa@gmail.com>
2021-01-29 21:22:42 +0100

    added media queries

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
-window.onload = addAutoplay();
+window.onload = manageAutoplay();
 
-function addAutoplay() {
-  let videoLocation = document.getElementById("myVideo");
-  let container = document.querySelector(".video-container");
+function manageAutoplay() {
+  const videoLocation = document.getElementById("myVideo");
+  const container = document.querySelector(".video-container");
   if (window.innerWidth > 1024) {
     videoLocation.removeAttribute("preload");
     videoLocation.setAttribute("autoplay", "");
     container.style.display = "block";
+  } else {
+    videoLocation.setAttribute("preload", "none");
+    videoLocation.removeAttribute("autoplay");
+    container.style.display = "none";
   }
 }
+
+window.addEventListener("resize", manageAutoplay);
