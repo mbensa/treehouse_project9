6a10ee46cd4eeb95fc5217b75f7487ac7336e511
Mojca Bensa <mojca.bensa@gmail.com>
2021-02-23 21:12:18 +0100

    added overlay images

--- js/scripts.js
+++ js/scripts.js
@@ -28,6 +28,7 @@ projectsSection.addEventListener("click", (e) => {
   }
 });
 
+let slideIndex = 1;
 function displayProject(modalId) {
   const projectOverlay = document.querySelector(".project-overlay");
   const modal = document.getElementById(modalId);
@@ -40,4 +41,37 @@ function displayProject(modalId) {
     projectOverlay.classList.add("hidden");
     modal.classList.add("hidden");
   });
+
+  showSlides(slideIndex);
+}
+
+function showSlides(n) {
+  const modals = document.querySelectorAll(".modal");
+  let activeModal;
+
+  modals.forEach((item) => {
+    if (!item.classList.contains("hidden")) {
+      activeModal = item;
+    }
+  });
+
+  if (activeModal) {
+    var i;
+    var slides = activeModal.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+  }
+}
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
 }
